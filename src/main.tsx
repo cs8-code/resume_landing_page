d4be1283b6e9c4aaebc3ff308a3535bd80e7cb8b
cs8-code <cs8121995@googlemail.com>
2025-11-18 13:44:41 +0100

    Add Vercel Speed Insights integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { LanguageProvider } from './context/LanguageContext';
 import { ErrorBoundary } from './components/ErrorBoundary';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights></SpeedInsights>
     <ErrorBoundary>
       <LanguageProvider>
         <App />
